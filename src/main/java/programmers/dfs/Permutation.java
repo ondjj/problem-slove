5badf7e1812dcb051d0a0b623d2c589f18372425
Chang-Jun Jeon <jcjk0302@gmail.com>
2024-01-20 16:56:32 +0900

    problem-solve(dfs) : permutation

---
+++ src/main/java/programmers/dfs/Permutation.java
@@ -0,0 +1,39 @@
+package programmers.dfs;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Permutation {
+    private static int n,m;
+    private static int[] list, check;
+
+    public void process(int level, int[] arr) {
+        if (level == m) {
+            System.out.println(Arrays.toString(list));
+        }
+        else{
+            for (int i = 0; i < n; i++) {
+                if (check[i] == 0) {
+                    check[i] = 1;
+                    list[level] = arr[i];
+                    process(level + 1, arr);
+                    check[i] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Permutation permutation = new Permutation();
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        m = scanner.nextInt();
+        int[] arr = new int[n];
+        check = new int[n];
+        list = new int[m];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        permutation.process(0, arr);
+    }
+}
